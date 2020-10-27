18cde9f8498e1e4361971dd8dfd9373e048edca6
Daniel Evans <dev@microsoft.com>
2020-10-26 18:31:28 -0700

    Add project files.

---
+++ Catering/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catering.Models
+{
+    public enum Card : int
+    {
+        Entre = 0,
+        Drink = 1,
+        Review = 2,
+        ReviewAll = 3,
+        Confirmation = 4,
+        OkWithString = 5,
+        OkWithCard = 6, // refreshes to a blank card, need to make a real card for this
+        LoginRequest = 7,
+        ThrottleWarning = 8, // got a 500 from directline??
+        Teapot = 9, // Got a 502 back from directline
+        Error = 10,
+        ErrMenu = 11
+    }
+}
