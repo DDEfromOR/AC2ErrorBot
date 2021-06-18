3461fa625c7753e51d7364dd2bc46cbd5c7331f8
Jeff Derstadt <jeffders@microsoft.com>
2021-06-18 15:37:05 -0700

    Update to latest and added simpler normal case

--- Catering/Bots/CateringBot.cs
+++ Catering/Bots/CateringBot.cs
@@ -1,28 +1,20 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Schema;
-using AdaptiveCards.Templating;
-using Newtonsoft.Json;
+using AdaptiveCards;
 using Catering.Cards;
 using Catering.Models;
-using System.Net;
-using AdaptiveCards;
-using Newtonsoft.Json.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using Microsoft.Bot.AdaptiveCards;
-using System.Linq;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Azure.Cosmos.Linq;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Bot.Builder;
 using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Catering
 {
@@ -37,24 +29,18 @@ namespace Catering
     // class is created. Objects that are expensive to construct, or have a lifetime
     // beyond the single turn, should be carefully managed.
 
-    public class CateringBot<TDialog> : ActivityHandler where TDialog : Dialog
+    public class CateringBot : ActivityHandler
     {
-        private const string WelcomeText = "Welcome to the Adaptive Cards 2.0 Bot. This bot will introduce you to Action.Execute in Adaptive Cards.";
+        private const string WelcomeText = "Welcome. This bot will introduce you to Action.Execute in Adaptive Cards.";
         private BotState _userState;
-        private CateringDb _cateringDb;
-        private readonly CateringRecognizer _cateringRecognizer;
-        private readonly Dialog _dialog;
         private readonly AdaptiveCardOAuthHandler _botAppHandler = new AdaptiveCardOAuthHandler("BotApp", "Sign-In To Bot App", "Sign-In");
         private readonly AdaptiveCardOAuthHandler _nonSsoHandler = new AdaptiveCardOAuthHandler("NonSSOBotApp", "Sign-In To Bot App", "Sign-In");
         private readonly IConfiguration _configuration;
 
-        public CateringBot(IConfiguration configuration, UserState userState, CateringDb cateringDb, CateringRecognizer cateringRecognizer, TDialog dialog)
+        public CateringBot(IConfiguration configuration, UserState userState)
         {
             _configuration = configuration;
             _userState = userState;
-            _cateringDb = cateringDb;
-            _cateringRecognizer = cateringRecognizer;
-            _dialog = dialog;
 
             var oauthCredential = new MicrosoftAppCredentials(
                 _configuration.GetSection("MicrosoftAppId")?.Value,
@@ -79,16 +65,7 @@ namespace Catering
         }
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            await _dialog.RunAsync(turnContext, _userState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
-        }
-        protected override async Task OnEndOfConversationActivityAsync(ITurnContext<IEndOfConversationActivity> turnContext, CancellationToken cancellationToken)
-        {
-            await _dialog.RunAsync(turnContext, _userState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
-        }
-
-        protected override async Task OnEventActivityAsync(ITurnContext<IEventActivity> turnContext, CancellationToken cancellationToken)
-        {
-            await _dialog.RunAsync(turnContext, _userState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+            await turnContext.SendActivityAsync(MessageFactory.Attachment((new CardResource("ErrorOptions.json").AsAttachment())));
         }
 
         protected override async Task<InvokeResponse> OnInvokeActivityAsync(ITurnContext<IInvokeActivity> turnContext, CancellationToken cancellationToken)
@@ -145,141 +122,88 @@ namespace Catering
                     return CreateInvokeResponse(HttpStatusCode.BadRequest, new Error("400", $"Received an invoke with name ${turnContext.Activity.Name} but not as a result of a loginRequest"));
                 }
             }
-            else if (AdaptiveCardInvokeValidator.IsAdaptiveCardAction(turnContext))
+            else
             {
-                var userSA = _userState.CreateProperty<User>(nameof(User));
-                var user = await userSA.GetAsync(turnContext, () => new User() { Id = turnContext.Activity.From.Id });
+                return await base.OnInvokeActivityAsync(turnContext, cancellationToken);
+            }
+        }
 
-                try
+        protected override async Task<AdaptiveCardInvokeResponse> OnAdaptiveCardInvokeAsync(ITurnContext<IInvokeActivity> turnContext, AdaptiveCardInvokeValue invokeValue, CancellationToken cancellationToken)
+        {
+            var userSA = _userState.CreateProperty<User>(nameof(User));
+            var user = await userSA.GetAsync(turnContext, () => new User() { Id = turnContext.Activity.From.Id });
+
+            var oauthCommandsProperty = _userState.CreateProperty<OAuthCommands>(nameof(OAuthCommands));
+            var oauthCommands = await oauthCommandsProperty.GetAsync(turnContext, () => new OAuthCommands() { StartedNominalAuth = false, StartedSSOAuth = false });
+
+            try
+            {
+                if (invokeValue.Action?.Data == null)
+                    throw new ArgumentNullException("invokeValue.Action.Data");
+
+                var cardOptions = ((JObject)invokeValue.Action.Data).ToObject<CardOptions>();
+
+                if (invokeValue.Action.Verb == "next")
                 {
-                    AdaptiveCardInvoke request = AdaptiveCardInvokeValidator.ValidateRequest(turnContext);
-                    var cardOptions = AdaptiveCardInvokeValidator.ValidateAction<CardOptions>(request);
+                    // show another card
+                    return CardResponse("BlandCard.json");
+                }
+                else if(invokeValue.Action.Verb == "back")
+                {
+                    // show another card
+                    return CardResponse("ErrorOptions.json");
+                }
+                else if (invokeValue.Action.Verb == "err")
+                {
+                    return await ProcessErrAction(user, cardOptions);
+                }
+                else if (invokeValue.Action.Verb == "nominal-oauth")
+                {
+                    oauthCommands.StartedNominalAuth = true;
+                    await oauthCommandsProperty.SetAsync(turnContext, oauthCommands);
 
-                    if (request.Action.Verb == "order")
+                    return await ProcessNominalOAuth(turnContext);
+                }
+                else if (invokeValue.Action.Verb == "sso-oauth")
+                {
+                    if (invokeValue.Authentication != null && !string.IsNullOrEmpty(invokeValue.Authentication.Token))
                     {
-                        // process action
-                        var responseBody = await ProcessOrderAction(user, cardOptions);
-
-                        return CreateInvokeResponse(HttpStatusCode.OK, responseBody);
-                    }
-
-                    if (request.Action.Verb == "err")
-                    {
-                        var responseBody = await ProcessErrAction(user, cardOptions);
-
-                        return CreateInvokeResponse(HttpStatusCode.OK, responseBody);
-                    }
-                    else if (request.Action.Verb == "nominal-oauth")
-                    {
-                        oauthCommands.StartedNominalAuth = true;
+                        // already started the oauth flow
+                        oauthCommands.StartedSSOAuth = false;
                         await oauthCommandsProperty.SetAsync(turnContext, oauthCommands);
 
-                        var responseBody = await ProcessNominalOAuth(turnContext);
+                        var result = await _botAppHandler.GetUserTokenAsync(turnContext, _userState);
 
-                        return CreateInvokeResponse(HttpStatusCode.OK, responseBody);
-                    }
-                    else if (request.Action.Verb == "sso-oauth")
-                    {
-                        if (request.Authentication != null && !string.IsNullOrEmpty(request.Authentication.Token))
+                        return new AdaptiveCardInvokeResponse()
                         {
-                            // already started the oauth flow
-                            oauthCommands.StartedSSOAuth = false;
-                            await oauthCommandsProperty.SetAsync(turnContext, oauthCommands);
-
-                            var result = await _botAppHandler.GetUserTokenAsync(turnContext, _userState);
-
-                            return CreateInvokeResponse(HttpStatusCode.OK, new AdaptiveCardInvokeResponse()
-                            {
-                                StatusCode = 200,
-                                Type = AdaptiveCardsConstants.Message,
-                                Value = $"Completed SSO token exchange and now have a user token: ${result.TokenResponse.Token}"
-                            });
-                        }
-                        else
-                        {
-                            oauthCommands.StartedSSOAuth = true;
-                            await oauthCommandsProperty.SetAsync(turnContext, oauthCommands);
-
-                            var responseBody = await ProcessSSOOAuth(turnContext);
-
-                            return CreateInvokeResponse(HttpStatusCode.OK, responseBody);
-                        }
-                    }
-                    else if (request.Action.Verb == "signout")
-                    {
-                        var responseBody = await ProcessSignout(turnContext);
-
-                        return CreateInvokeResponse(HttpStatusCode.OK, responseBody);
+                            StatusCode = 200,
+                            Type = AdaptiveCardsConstants.Message,
+                            Value = $"Completed SSO token exchange and now have a user token: ${result.TokenResponse.Token}"
+                        };
                     }
                     else
                     {
-                        AdaptiveCardActionException.VerbNotSupported(request.Action.Type);
+                        oauthCommands.StartedSSOAuth = true;
+                        await oauthCommandsProperty.SetAsync(turnContext, oauthCommands);
+
+                        return await ProcessSSOOAuth(turnContext);
                     }
                 }
-                catch (AdaptiveCardActionException e)
+                else if (invokeValue.Action.Verb == "signout")
                 {
-                    return CreateInvokeResponse(HttpStatusCode.OK, e.Response);
+                    return await ProcessSignout(turnContext);
+                }
+                else
+                {
+                    AdaptiveCardActionException.VerbNotSupported(invokeValue.Action.Type);
                 }
             }
-
-            return null;
-        }
-
-        private async Task<AdaptiveCardInvokeResponse> ProcessOrderAction(User user, CardOptions cardOptions)
-        {
-            if ((Card)cardOptions.currentCard == Card.Entre)
+            catch (AdaptiveCardActionException e)
             {
-                if (!string.IsNullOrEmpty(cardOptions.custom))
-                {
-                    if (!await _cateringRecognizer.ValidateEntre(cardOptions.custom))
-                    {
-                        return RedoEntreCardResponse(new Lunch() { Entre = cardOptions.custom });
-                    }
-                    cardOptions.option = cardOptions.custom;
-                }
-
-                user.Lunch.Entre = cardOptions.option;
-            }
-            else if ((Card)cardOptions.currentCard == Card.Drink)
-            {
-                if (!string.IsNullOrEmpty(cardOptions.custom))
-                {
-                    if (!await _cateringRecognizer.ValidateDrink(cardOptions.custom))
-                    {
-                        return RedoDrinkCardResponse(new Lunch() { Drink = cardOptions.custom });
-                    }
-
-                    cardOptions.option = cardOptions.custom;
-                }
-
-                user.Lunch.Drink = cardOptions.option;
+                return e.Response;
             }
 
-            AdaptiveCardInvokeResponse responseBody = null;
-            switch ((Card)cardOptions.nextCardToSend)
-            {
-                case Card.Drink:
-                    responseBody = DrinkCardResponse();
-                    break;
-                case Card.Entre:
-                    responseBody = EntreCardResponse();
-                    break;
-                case Card.Review:
-                    responseBody = ReviewCardResponse(user);
-                    break;
-                case Card.ReviewAll:
-                    var latestOrders = await _cateringDb.GetRecentOrdersAsync();
-                    responseBody = RecentOrdersCardResponse(latestOrders.Items);
-                    break;
-                case Card.Confirmation:
-                    await _cateringDb.UpsertOrderAsync(user);
-                    responseBody = ConfirmationCardResponse();
-                    break;
-                default:
-                    throw new NotImplementedException("No card matches that nextCardToSend.");
-            }
-
-            return responseBody;
+            throw new InvokeResponseException(HttpStatusCode.NotImplemented);
         }
 
         private async Task<AdaptiveCardInvokeResponse> ProcessErrAction(User user, CardOptions cardOptions)
@@ -388,62 +312,6 @@ namespace Catering
             };
         }
 
-        private AdaptiveCardInvokeResponse CardResponse<T>(string cardFileName, T data)
-        {
-            return new AdaptiveCardInvokeResponse()
-            {
-                StatusCode = 200,
-                Type = AdaptiveCard.ContentType,
-                Value = new CardResource(cardFileName).AsJObject(data)
-            };
-        }
-
-        private AdaptiveCardInvokeResponse DrinkCardResponse()
-        {
-            return CardResponse("DrinkOptions.json");
-        }
-
-        private AdaptiveCardInvokeResponse EntreCardResponse()
-        {
-            return CardResponse("EntreOptions.json");
-        }
-
-        private AdaptiveCardInvokeResponse ReviewCardResponse(User user)
-        {
-            return CardResponse("ReviewOrder.json", user.Lunch);
-        }
-
-        private AdaptiveCardInvokeResponse RedoDrinkCardResponse(Lunch lunch)
-        {
-            return CardResponse("RedoDrinkOptions.json", lunch);
-        }
-
-        private AdaptiveCardInvokeResponse RedoEntreCardResponse(Lunch lunch)
-        {
-            return CardResponse("RedoEntreOptions.json", lunch);
-        }
-
-        private AdaptiveCardInvokeResponse RecentOrdersCardResponse(IList<User> users)
-        {
-            return CardResponse("RecentOrders.json",
-                new
-                {
-                    users = users.Select(u => new
-                    {
-                        lunch = new
-                        {
-                            entre = u.Lunch.Entre,
-                            drink = u.Lunch.Drink,
-                            orderTimestamp = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(u.Lunch.OrderTimestamp, "Pacific Standard Time").ToString("g")
-                        }
-                    }).ToList()
-                });
-        }
-
-        private AdaptiveCardInvokeResponse ConfirmationCardResponse()
-        {
-            return CardResponse("Confirmation.json");
-        }
 
         #endregion
     
--- Catering/Models/Card.cs
+++ Catering/Models/Card.cs
@@ -7,11 +7,6 @@ namespace Catering.Models
 {
     public enum Card : int
     {
-        Entre = 0,
-        Drink = 1,
-        Review = 2,
-        ReviewAll = 3,
-        Confirmation = 4,
         OkWithString = 5,
         OkWithCard = 6, // refreshes to a blank card, need to make a real card for this
         ThrottleWarning = 8, // got a 500 from directline??
